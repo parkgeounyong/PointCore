34592ff06519ffd894ea490bd138097eddbb7765
gyparkCap <gy.park.cap@gmail.com>
2026-02-23 18:18:03 +0900

    first commit

---
+++ src/main/kotlin/io/github/parkgeounyong/point_core/PointCoreApplication.kt
@@ -0,0 +1,11 @@
+package io.github.parkgeounyong.point_core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<PointCoreApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/parkgeounyong/point_core/PointCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.parkgeounyong.point_core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PointCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
